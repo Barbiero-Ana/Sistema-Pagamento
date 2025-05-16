1977d71cfb9d7217bf126148de46baaac4647726
Ana Barbiero <anacarolinabarbiero@gmail.com>
2025-05-16 18:39:49 -0400

    Update front.py

--- front.py
+++ front.py
@@ -1,4 +1,6 @@
 import streamlit as st
 
-st
 st.title('Sistema de pagamentos')
+st.header('Sistema de gerenciamento de pagamentos ')
+st.sidebar.header('Opções disponiveis')
+st.sidebar.selectbox('escolha a opcao que deseja', ['opcao1', 'opcao2', 'opcao3'])
